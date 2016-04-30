0035fa4500365e3180a09b6260fbfdaeb79f1ac8
Chris Brinson <chrisjbrinson@gmail.com>
2016-04-30 17:10:08 -0600

    Wx Descriptions
    Adding the possible weather descriptions the api will throw so the
    background picture will correspond with the weather

--- weather.js
+++ weather.js
@@ -22,35 +22,44 @@ var date = new Date;
 	      	$(".display-3").html(json["current_observation"]["display_location"]["full"]);
 	      	$(".lead").html(c + " &#8451 " + " | " + f + " &#8457<br>" + description + "<br> Local Time: " + date);
 	      	
-
+	      	alert("Your browser does not support this.");
 	      	//console.log(json["current_observation"]["full"]);
 
 //if / else if statements to determine what picture should populate the background
-	      	if(description === "CLEAR SKY"){
+	      	if(description === "CLEAR"){
 		      	$('body').css('background-image', 'url(images/clear.jpg)');
-		      	} else if (description === "SCATTERED CLOUDS") {
+		      	} else if (description.indexOf("SCATTERED") >= 0 || description.indexOf("PARTLY") >= 0 ) {
 		      		$('body').css('background-image', 'url(images/scattered.jpg)');
 		      		document.getElementById("test").style.opacity = "0.6";
-		      	} else if (description === "FEW CLOUDS"){
-		      		$('body').css('background-image', 'url(images/scattered.jpg)');
-		      		document.getElementById("test").style.opacity = "0.6";
-		      	} else if(description === "BROKEN CLOUDS") {
+		      	} else if(description.indexOf("MOSTLY") >= 0) {
 		      		$('body').css('background-image', 'url(images/broken.jpg)');
-		      	}else if (description === "OVERCAST CLOUDS"){
+		      	}else if (description === "OVERCAST"){
 		      		$('body').css('background-image', 'url(images/overcast.jpg)');
+		      	} else if (description.indexOf("THUNDERSTORM") >= 0 || description.indexOf("HAIL") >= 0){
+		      		$('body').css('background-image', 'url(images/thunderstorm.jpg)');
+		      		document.getElementById("test").style.opacity = "0.8";
 		      	} else if (description.indexOf("SNOW") >= 0){
 		      		$('body').css('background-image', 'url(images/snow.jpg)');
 		      		document.getElementById("test").style.opacity = "0.8";
-		      	} else if (description === "SHOWER RAIN"){
+		      	} else if (description.indexOf("RAIN") >= 0 || description.indexOf("DRIZZLE") >= 0){
 		      		$('body').css('background-image', 'url(images/rain.jpg)');
 		      		document.getElementById("test").style.opacity = "0.6";
-
-	      		
-	      	} 
+		      	}  else if (description.indexOf("FOG") >= 0 || description.indexOf("MIST") >= 0){
+		      		$('body').css('background-image', 'url(images/fog.jpg)');
+		      	} else if (description.indexOf("SMOKE") >= 0 || description.indexOf("HAZE") >= 0){
+		      		$('body').css('background-image', 'url(images/haze.jpg)');
+		      	} else if (description.indexOf("VOLCANIC") >= 0){
+		      		$('body').css('background-image', 'url(images/volcano.jpg)');
+		      	} else if (description.indexOf("SAND") >= 0 || description.indexOf("DUST") >= 0){
+		      		$('body').css('background-image', 'url(images/sand.jpg)');
+		      	}
+	      	 
 	     
 	      });
 	    });
-	  } 
+	  } else {
+	  	alert("Your browser does not support this.");
+	  }
 	  
 	//});
   
